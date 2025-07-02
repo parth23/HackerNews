6a461ff32395b1c5e0c5f842e3b8d6b7739e01fb
Parth Savadiya <psavadiy@cisco.com>
2025-07-02 22:15:48 +0530

    feat: Update HackerNews service to use environment variables for API URL and add deployment configuration

--- HackerNewsUI/src/app/services/hacker-news.service.ts
+++ HackerNewsUI/src/app/services/hacker-news.service.ts
@@ -3,12 +3,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { HackerNewsStory, PaginatedResult, StoryQueryParameters } from '../models/hacker-news.models';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HackerNewsService {
-  public readonly apiUrl = 'http://localhost:5109/api/stories';
+  private apiUrl = `${environment.apiUrl}/stories`;
 
   constructor(private http: HttpClient) { }
 
